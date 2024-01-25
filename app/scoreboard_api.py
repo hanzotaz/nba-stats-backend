2c4b197f2fa69a2addc254d644228a98fd404a08
Asyeeq Asywaree <asyeeqasywaree@gmail.com>
2024-01-25 21:15:49 +0800

    added score fields to scoreboard

--- app/scoreboard_api.py
+++ app/scoreboard_api.py
@@ -1,11 +1,34 @@
+import json
+from datetime import timezone
+from dateutil import parser
 from nba_api.live.nba.endpoints import scoreboard
 
-class NBALivesScoreBoard():
+
+class NBALivesScoreBoard:
     def __init__(self):
-        self.games = scoreboard.ScoreBoard()
+        self.score = scoreboard.ScoreBoard()
 
     def get_all_scoreboards(self):
         try:
-            return self.games.get_json()
+            games = self.score.games.get_dict()
+            scoreboardDict = {"date": self.score.score_board_date, "games": {}}
+
+            for game in games:
+                gameTimeLTZ = (
+                    parser.parse(game["gameTimeUTC"])
+                    .replace(tzinfo=timezone.utc)
+                    .astimezone(tz=None)
+                )
+
+                game_info = {
+                    "gameTimeLTZ": gameTimeLTZ.isoformat(),
+                    "homeTeam": game["homeTeam"]["teamName"],
+                    "homeScore": game["homeTeam"]["score"],
+                    "awayTeam": game["awayTeam"]["teamName"],
+                    "awayScore": game["awayTeam"]["score"],
+                }
+
+                scoreboardDict["games"][game["gameId"]] = game_info
+            return json.dumps(scoreboardDict)
         except Exception as e:
             return {"error": f"Error retrieving live scoreboards: {str(e)}"}
